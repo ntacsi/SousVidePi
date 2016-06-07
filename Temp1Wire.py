5dd27271cb69f5fffece0924c482db56e2f9f5b5
ntacsi <ntacsi@gmail.com>
2016-06-07 18:52:13 +0200

    fixed tab issue with Temp1Wire

--- Temp1Wire.py
+++ Temp1Wire.py
@@ -10,12 +10,11 @@ class Temp1Wire:
     def readTempC(self):
         pipe = Popen(["cat", self.oneWireDir + self.tempSensorId + "/w1_slave"], stdout=PIPE)
         result = pipe.communicate()[0]
-
-		try:
-			if result.split('\n')[0].split(' ')[11] == "YES":
-				temp_C = float(result.split("=")[-1]) / 1000  # temp in Celcius
-			else:
-				temp_C = -99  # bad temp reading
+        try:
+            if result.split('\n')[0].split(' ')[11] == "YES":
+                temp_C = float(result.split("=")[-1]) / 1000  # temp in Celcius
+            else:
+                temp_C = -99  # bad temp reading
         except:
             print("Failed to read temperature from file")
 
